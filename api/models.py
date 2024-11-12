6184f55c1d9f05268f73c207905a2898e1069cf9
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-11-12 18:44:52 -0300

    fix: change timestamp names

--- api/models.py
+++ api/models.py
@@ -10,14 +10,14 @@ class Tarefa(postgres_db.Model):
     id = postgres_db.Column(postgres_db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     titulo = postgres_db.Column(postgres_db.String(120), nullable=False)
     descricao = postgres_db.Column(postgres_db.String(255), nullable=False)
-    data_criacao = postgres_db.Column(postgres_db.DateTime, default=datetime.now(brt_timezone))
-    data_exclusao = postgres_db.Column(postgres_db.DateTime, nullable=True)
+    momento_criacao = postgres_db.Column(postgres_db.DateTime, default=datetime.now(brt_timezone))
+    momento_conclusao = postgres_db.Column(postgres_db.DateTime, nullable=True)
 
     def to_dict(self):
         return {
             "id": self.id,
             "titulo": self.titulo,
             "descricao": self.descricao,
-            "data_criacao": self.data_criacao,
-            "data_exclusao": self.data_exclusao
+            "momento_criacao": self.momento_criacao,
+            "momento_conclusao": self.momento_conclusao
         }
