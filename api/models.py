028b063d190bdece4e37539352b89d956afb8c24
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-11-12 19:43:49 -0300

    feat: add 'concluido' model attribute

--- api/models.py
+++ api/models.py
@@ -10,6 +10,7 @@ class Tarefa(postgres_db.Model):
     id = postgres_db.Column(postgres_db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     titulo = postgres_db.Column(postgres_db.String(120), nullable=False)
     descricao = postgres_db.Column(postgres_db.String(255), nullable=False)
+    concluido = postgres_db.Column(postgres_db.Boolean, default=False)
     momento_criacao = postgres_db.Column(postgres_db.DateTime, default=datetime.now(brt_timezone))
     momento_conclusao = postgres_db.Column(postgres_db.DateTime, nullable=True)
 
@@ -18,6 +19,7 @@ class Tarefa(postgres_db.Model):
             "id": self.id,
             "titulo": self.titulo,
             "descricao": self.descricao,
+            "concluido": self.concluido,
             "momento_criacao": self.momento_criacao,
             "momento_conclusao": self.momento_conclusao
         }
