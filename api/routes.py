5b2a33a1e1c3cdac2b6a513f3ba2344a5c7b931b
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-11-12 12:26:08 -0300

    fix: set development Postgres for TDD test and 'criar_tarefa' POST for '/tarefas' route

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from .models import postgres_db, Tarefa
 
 tarefas = Blueprint('tarefas', __name__)
-
-mock_tarefas = []
 
 @tarefas.route('/tarefas', methods=['POST'])
 def criar_tarefa():
@@ -12,6 +12,7 @@ def criar_tarefa():
     if not titulo or not descricao:
         return jsonify({"error": "Título e descrição são obrigatórios"}), 400
 
-    nova_tarefa = {"titulo": titulo, "descricao": descricao}
-    mock_tarefas.append(nova_tarefa)
-    return jsonify(nova_tarefa), 201
+    nova_tarefa = Tarefa(titulo=titulo, descricao=descricao)
+    postgres_db.session.add(nova_tarefa)
+    postgres_db.session.commit()
+    return jsonify(nova_tarefa.to_dict()), 201
--- api/test_tarefas.py
+++ api/test_tarefas.py
@@ -1,15 +1,31 @@
-import pytest
+import pytest, os
 from flask import Flask
+from flask_migrate import Migrate
 from .routes import tarefas as tarefas_blueprint
+from .models import postgres_db, Tarefa
 
 @pytest.fixture
 def client():
     app = Flask(__name__)
     app.register_blueprint(tarefas_blueprint)  
-    app.config['TESTING'] = True 
-    
+        
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    postgres_db.init_app(app)
+    Migrate().init_app(app, postgres_db)
+
+    with app.app_context():
+        postgres_db.create_all()  # Cria as tabelas no banco de dados
+
+    # Criando o cliente de teste
     with app.test_client() as client:
-        yield client 
+        yield client  # O cliente é fornecido para os testes
+
+    # Limpeza do banco após os testes
+    with app.app_context():
+        postgres_db.drop_all()  # Remove as tabelas do banco de dados
+            
         
 def test_criar_tarefa(client):
     response = client.post('/tarefas', json = {"titulo": "Titulo Teste 1", "descricao": "Descricao Teste 1"})
