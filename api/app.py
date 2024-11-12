abf56f9c2fc190ec5a6a55382f5ab584c18ab09a
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-11-12 10:31:25 -0300

    feat: create application and model layers, configure postgres database

--- api/app.py
+++ api/app.py
@@ -1,11 +1,30 @@
 from flask import Flask
-from routes import tarefas as tarefas_blueprint
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import os 
+from .routes import tarefas as tarefas_blueprint
+
+postgres_db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(tarefas_blueprint)
     
+    configure_app(app)
+    initialize_postgres(app)    
+    migrate.init_app(app, postgres_db)
+    
+    with app.app_context():
+        postgres_db.create_all()
     return app
+
+def configure_app(_app):    
+    _app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI')
+    _app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+def initialize_postgres(_app):
+    postgres_db.init_app(_app)
 
 if __name__ == '__main__':
     app = create_app()
---
+++ api/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone, timedelta
+import uuid
+
+postgres_db = SQLAlchemy()
+brt_timezone = timezone(timedelta(hours=-3))
+
+class Tarefa(postgres_db.Model):
+    __tablename__ = 'tarefas'
+    
+    id = postgres_db.Column(postgres_db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    titulo = postgres_db.Column(postgres_db.String(120), nullable=False)
+    descricao = postgres_db.Column(postgres_db.String(255), nullable=False)
+    data_criacao = postgres_db.Column(postgres_db.DateTime, default=datetime.now(brt_timezone))
+    data_exclusao = postgres_db.Column(postgres_db.DateTime, nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "titulo": self.titulo,
+            "descricao": self.descricao,
+            "data_criacao": self.data_criacao,
+            "data_exclusao": self.data_exclusao
+        }
