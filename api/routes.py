5019853c3cb6f03d18a8254ea0bea3b7e6ed2967
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-11-13 08:39:35 -0300

    feat: patch and tests

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone, timedelta
 from .models import postgres_db, Tarefa
 
+brt_timezone = timezone(timedelta(hours=-3))
 tarefas = Blueprint('tarefas', __name__)
 
 @tarefas.route('/tarefas', methods=['POST'])
@@ -19,23 +20,34 @@ def criar_tarefa():
 
 @tarefas.route('/tarefas', methods=['GET'])
 def listar_tarefas():
-    data = postgres_db.session.query(Tarefa).all()
+    status = request.args.get('status')  
+    query = postgres_db.session.query(Tarefa)
+    
+    if status == 'incompleto':
+        query = query.filter(Tarefa.concluido == False)
+        
+    data = query.all()
     return jsonify([tarefa.to_dict() for tarefa in data]), 200
     
 @tarefas.route('/tarefas', methods=['PATCH'])
 def alterar_tarefa():
     data = request.get_json()
     tarefa_id = data.get('id')
-    tarefa = postgres_db.session.get(Tarefa, tarefa_id)  
+    tarefa = postgres_db.session.get(Tarefa, tarefa_id)
 
     if not tarefa:
-        return jsonify({"error": "Tarefa não encontrada"}), 404 
-    
+        return jsonify({"error": "Tarefa não encontrada"}), 404
+
     if 'titulo' in data and data['titulo']:
         tarefa.titulo = data['titulo']
+    
     if 'descricao' in data and data['descricao']:
         tarefa.descricao = data['descricao']
-        
+    
+    if 'concluido' in data:
+        tarefa.concluido = data['concluido']
+        tarefa.momento_conclusao = datetime.now(brt_timezone) if tarefa.concluido else None
+
     postgres_db.session.commit()
     return jsonify(tarefa.to_dict()), 200
 
@@ -45,9 +57,10 @@ def excluir_tarefa():
     tarefa_id = data.get('id')
     tarefa = postgres_db.session.get(Tarefa, tarefa_id)  
     if not tarefa:
-        return jsonify({"error": "Tarefa não encontrada"}), 404  # Retorna erro se a tarefa não existir
+        return jsonify({"error": "Tarefa não encontrada"}), 404  
 
     postgres_db.session.delete(tarefa) 
     postgres_db.session.commit() 
 
     return jsonify(tarefa.to_dict()), 200 
+
--- api/test_tarefas.py
+++ api/test_tarefas.py
@@ -51,6 +51,7 @@ def test_criar_tarefa_descricao_vazio(client):
     assert response.json == {"error": "Título e descrição são obrigatórios"}
     
 def test_listar_tarefas(client):
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste lista", "descricao": "Descricao Teste lista"})
     response = client.get('/tarefas')
     assert response.status_code == 200
     assert isinstance(response.json, list)
@@ -58,27 +59,69 @@ def test_listar_tarefas(client):
         assert 'titulo' in tarefa
         assert 'descricao' in tarefa
 
-def test_alterar_tarefa(client):
-    nova_tarefa = {
-        "titulo": "Titulo teste 0",
-        "descricao": "Descrição teste 0"
-    }
-    response = client.post('/tarefas', json=nova_tarefa)
-    assert response.status_code == 201
-    tarefa_id = response.json['id']  
-    
-    dados_atualizados = {
-        "id": tarefa_id,
-        "titulo": "Titulo teste 1",
-        "descricao": "Descrição teste 1"
-    }
-    response = client.patch('/tarefas', json=dados_atualizados)
-    assert response.status_code == 200
-    assert response.json['titulo'] == "Titulo teste 1"
-    assert response.json['descricao'] == "Descrição teste 1"
-    
-    import pytest
+def test_listar_tarefas_incompletas(client):
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste lista 1", "descricao": "Descricao Teste lista 1"})
+    tarefa_id_1 = response.get_json()["id"]
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste lista 2", "descricao": "Descricao Teste lista 2"})
+    tarefa_id_2 = response.get_json()["id"]
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste lista 3", "descricao": "Descricao Teste lista 3"})
 
+    client.patch('/tarefas', json = {"id": tarefa_id_1, "concluido": True})
+    client.patch('/tarefas', json = {"id": tarefa_id_2, "concluido": True})
+
+    response = client.get('/tarefas?status=incompleto')
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert len(response.json) == 1
+    for tarefa in response.json:
+        assert 'titulo' in tarefa
+        assert 'descricao' in tarefa
+        assert tarefa.get('concluido') == False  
+        
+def test_listar_tarefas_banco_vazio(client):
+    response = client.get('/tarefas')
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert response.get_json() == []
+        
+def test_alterar_tarefa(client):
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste 1", "descricao": "Descricao Teste 1"})
+    tarefa_id_1 = response.get_json()["id"]
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste 2", "descricao": "Descricao Teste 2"})
+    tarefa_id_2 = response.get_json()["id"]
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste 3", "descricao": "Descricao Teste 3"})
+    tarefa_id_3 = response.get_json()["id"]
+
+    response = client.patch('/tarefas', json = {"id": tarefa_id_1, "titulo": "Titulo Teste 1 - Atualizado", "concluido": True})
+    assert response.status_code == 200
+    assert response.get_json()['titulo'] == "Titulo Teste 1 - Atualizado"
+    assert response.get_json()['concluido'] == True
+    
+    response = client.patch('/tarefas', json = {"id": tarefa_id_2, "descricao": "Descricao Teste 2 - Atualizado", "concluido": True})
+    assert response.status_code == 200
+    assert response.get_json()['descricao'] == "Descricao Teste 2 - Atualizado"
+    assert response.get_json()['concluido'] == True
+    assert response.get_json()["momento_conclusao"] is not None
+    
+    response = client.patch('/tarefas', json = {"id": tarefa_id_3, "concluido": True})
+    assert response.status_code == 200
+    assert response.get_json()['concluido'] == True
+    assert response.get_json()["momento_conclusao"] is not None
+    
+def test_reabrir_tarefa(client):
+    response = client.post('/tarefas', json = {"titulo": "Titulo Teste 1", "descricao": "Descricao Teste 1"})
+    tarefa_id_1 = response.get_json()["id"]
+
+    response = client.patch('/tarefas', json={"id": tarefa_id_1, "concluido": True})
+    assert response.status_code == 200
+    assert response.get_json()["concluido"] is True
+    assert response.get_json()["momento_conclusao"] is not None
+
+    response = client.patch('/tarefas', json={"id": tarefa_id_1, "concluido": False})
+    assert response.status_code == 200
+    assert response.get_json()["concluido"] is False
+    assert response.get_json()["momento_conclusao"] is None
+    
 def test_excluir_tarefa(client):
     nova_tarefa = {
         "titulo": "Tarefa para Deletar",
@@ -91,3 +134,7 @@ def test_excluir_tarefa(client):
     response = client.delete('/tarefas', json={"id": tarefa_id})
     assert response.status_code == 200 
     assert response.json['id'] == tarefa_id  
+
+    response = client.get('/tarefas')
+    tasks = response.get_json()
+    assert not any(task["id"] == tarefa_id for task in tasks)
